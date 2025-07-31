c768560c08f3692d2a775db8e3c403f173be170c
your name <you@example.com>
2025-07-30 21:44:13 -0400

    Initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,10 +8,26 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ error, selectedDifficulty, selectedSport, isInQuiz }) => {
+  const handleLogoClick = () => {
+    // Reset the quiz state by refreshing the page
+    window.location.href = '/';
+  };
+
   return (
     <header className="App-header">
       <div className="logo-container">
-        <h1 style={{ fontWeight: 'bold', fontSize: '3.5em' }}>ScoreTrivia</h1>
+        <h1 
+          style={{ 
+            fontWeight: 'bold', 
+            fontSize: '3.5em',
+            cursor: 'pointer',
+            userSelect: 'none'
+          }}
+          onClick={handleLogoClick}
+          title="Click to go back to home"
+        >
+          ScoreTrivia
+        </h1>
       </div>
       <p className="subtitle" style={{ fontSize: '1.7em', marginTop: '-0.5em', marginBottom: '1em' }}>Unlimited Sports Quizzes</p>
       {/* Only show quiz info when actively taking a quiz */}
